714713b4f2708ffd2907f8a31893412e6038c470
chuy <pseudojzz2@gmail.com>
2018-01-09 23:27:26 -0600

    heheh

---
+++ Programa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "===================================================="
+echo "Esta es la IP interna"
+ip  -f inet a show eth0| grep inet| awk '{ print $2}' | cut -d/ -f1
+echo "===================================================="
+echo "Este es el espacio disponible y espacio disponible del disco(Exp en MB)"
+df -h
+echo "===================================================="
+echo "Esta es la RAM libre y la RAM usada(expresada en MB)"
+free -m
+echo "===================================================="
+echo "Esta es la version de OS instalada"
+cat /etc/redhat-release
+echo "===================================================="
+echo "Estas son las Reglas de IPtables"
+iptables -L
+echo "===================================================="
