6fa51e532358de98cd458ebb5688ffb05bf77cdd
jjllanes <psuedojzz2@gmail.com>
2018-01-11 03:55:22 +0000

    LAMP

---
+++ Programa2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+echo "                   Program to create LAMP stack CentOS 7"
+echo "==============================================================================="
+echo "                                                                               "
+echo "                                 APACHE"
+
+yum install httpd
+service httpd start 
+
+echo "==============================================================================="
+
+
+
